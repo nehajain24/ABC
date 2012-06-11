20f6b50d8329269619b818bbf28fa3909f497770
nehajain24 <neha.jain@akraya.com>
2012-06-11 12:38:24 +0530

    ADDING   NEW FIlES

---
+++ RTF_To_HTML_Demo_App/Backup/RTF_To_HTML_Demo_App/Form1.vb
@@ -0,0 +1,46 @@
+﻿Public Class Form1
+
+    Public Function sRTF_To_HTML(ByVal sRTF As String) As String
+
+        Dim MyWord As Microsoft.Office.Interop.Word.Application
+        Dim oDoNotSaveChanges As Object = Microsoft.Office.Interop.Word.WdSaveOptions.wdDoNotSaveChanges
+        Dim sReturnString As String = ""
+        Dim sConvertedString As String = ""
+        Try
+            MyWord = CreateObject("Word.application")
+            MyWord.Visible = False
+            MyWord.Documents.Add()
+
+
+            Dim doRTF As New System.Windows.Forms.DataObject
+            doRTF.SetData("Rich Text Format", sRTF)
+            Clipboard.SetDataObject(doRTF)
+            MyWord.Windows(1).Selection.Paste()
+            MyWord.Windows(1).Selection.WholeStory()
+            MyWord.Windows(1).Selection.Copy()
+            sConvertedString = Clipboard.GetData(System.Windows.Forms.DataFormats.Html)
+            'Remove some leading text that shows up in the email
+            sConvertedString = sConvertedString.Substring(sConvertedString.IndexOf("<html"))
+            'Also remove multiple Â characters that somehow got inserted 
+            sConvertedString = sConvertedString.Replace("Â", "")
+            sReturnString = sConvertedString
+            If Not MyWord Is Nothing Then
+                MyWord.Quit(oDoNotSaveChanges)
+                MyWord = Nothing
+            End If
+        Catch ex As Exception
+            If Not MyWord Is Nothing Then
+                MyWord.Quit(oDoNotSaveChanges)
+                MyWord = Nothing
+            End If
+            MsgBox("Error converting Rich Text to HTML")
+        End Try
+        Return sReturnString
+    End Function
+
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Me.TextBox1.Text = Me.sRTF_To_HTML(Me.RichTextBox1.Rtf)
+    End Sub
+
+End Class
---
+++ RTF_To_HTML_Demo_App/Backup/RTF_To_HTML_Demo_App/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("RTF_To_HTML_Demo_App")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("LRC")> 
+<Assembly: AssemblyProduct("RTF_To_HTML_Demo_App")> 
+<Assembly: AssemblyCopyright("Copyright © LRC 2010")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("525f17d1-571f-494d-be46-ffe278516cf6")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
